6386153880cdf81d28c4e9db8487be01b46e3239
Sampath Kumar <sam1990kumar@gmail.com>
2016-01-24 17:58:42 +0530

    First commit

---
+++ app/models.py
@@ -0,0 +1,65 @@
+from flask.ext.appbuilder import Model
+from flask.ext.appbuilder.models.mixins import AuditMixin, FileColumn, ImageColumn
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+"""
+
+You can use the extra Flask-AppBuilder fields and Mixin's
+
+AuditMixin will add automatic timestamp of created and modified by who
+
+
+"""
+class ContactGroup(Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique = True, nullable=False)
+
+    def __repr__(self):
+        return self.name
+
+
+class projects(Model):
+    id = Column(Integer, primary_key=True)
+    name =  Column(String(150), unique = True, nullable=False)
+    description =  Column(String(500), unique = True, nullable=False)
+
+    def __repr__(self):
+        return 'Project:%r' % self.name
+
+class resources(Model):
+    id = Column(Integer, primary_key=True)
+    project_id = Column(Integer, ForeignKey('projects.id'))
+    name =  Column(String(150), unique = True, nullable=False)
+    description =  Column(String(500), unique = True, nullable=False)
+    
+    # relation ship
+    project_name = relationship("projects")
+
+    def __repr__(self):
+        return '%s:%r' % (self.project_name, self.name)
+
+class resources_availability(Model):
+    id = Column(Integer, primary_key=True)
+    resource_id = Column(Integer, ForeignKey('resources.id'))
+    start_time = DateTime()
+    end_time =DateTime()
+
+    # relationship
+    resource_name = relationship('resources')
+
+    def __repr__(self):
+        return '<resources_availability id:%r>' % self.id
+
+class resources_booking(Model):
+    id = Column(Integer, primary_key=True)
+    resource_id = Column(Integer, ForeignKey('resources.id'))
+    start_time = DateTime()
+    end_time =DateTime()
+
+    # relationship
+    resource_name = relationship('resources')
+
+    def __repr__(self):
+        return '<resources_booking id:%r>' % self.id
+
+
---
+++ app/views.py
@@ -0,0 +1,115 @@
+from flask.ext.appbuilder import AppBuilder, BaseView, expose, has_access
+from flask import flash, render_template
+from flask_appbuilder import SimpleFormView
+from flask.ext.babelpkg import lazy_gettext as _
+
+# class model views
+from flask.ext.appbuilder import ModelView
+from flask.ext.appbuilder.models.sqla.interface import SQLAInterface
+
+from app import db, appbuilder
+from forms import MyForm
+
+from models import ContactGroup
+from models import projects, resources
+from models import resources_availability, resources_booking
+
+class MyView(BaseView):
+
+    default_view = 'method1'
+
+    @expose('/method1/')
+    @has_access
+    def method1(self):
+        # do something with param1
+        # and return to previous page or index
+        return 'Hello'
+
+    @expose('/method2/<string:param1>')
+    @has_access
+    def method2(self, param1):
+        # do something with param1
+        # and render template with param
+        param1 = 'Goodbye %s' % (param1)
+        return param1
+
+    @expose('/method3/<string:param1>')
+    @has_access
+    def method3(self, param1):
+        # do something with param1
+        # and render template with param
+        param1 = 'Goodbye %s' % (param1)
+        self.update_redirect()
+        return self.render_template('method3.html',param1=param1)
+
+class MyFormView(SimpleFormView):
+    form = MyForm
+    form_title = 'This is my first form view'
+    message = 'My form submitted'
+
+    def form_get(self, form):
+        form.field1.data = 'this was pre-filled data'
+
+    def form_post(self, form):
+        # post process form
+        flash(self.message, 'info')
+
+
+
+class resources_availability_model_view(ModelView):
+    datamodel = SQLAInterface(resources_availability)
+
+class resources_booking_model_view(ModelView):
+    datamodel = SQLAInterface(resources_booking)
+
+class resources_model_view(ModelView):
+    datamodel = SQLAInterface(resources)
+    related_views = [
+            resources_availability_model_view,
+            resources_booking_model_view,
+            ]
+
+class projects_model_view(ModelView):
+    datamodel = SQLAInterface(projects)
+    list_columns = [ 'name', 'description']
+    related_views = [
+            resources_model_view,
+            ]
+    
+    
+appbuilder.add_view(MyView, "Method1", category='My View')
+appbuilder.add_link("Method2", href='/myview/method2/john', category='My View')
+appbuilder.add_link("Method3", href='/myview/method3/john', category='My View')
+# FORM Submit
+appbuilder.add_view(MyFormView, "My form View", icon="fa-group", \
+        label=_('My form View'), \
+        category="My Forms", \
+        category_icon="fa-cogs")
+
+
+class GroupModelView(ModelView):
+    datamodel = SQLAInterface(ContactGroup)
+    # related_views = [ContactModelView]
+
+
+db.create_all()
+appbuilder.add_view(GroupModelView, "List Groups",
+    icon = "fa-folder-open-o",
+    category = "Contacts",
+    category_icon = "fa-envelope")
+
+appbuilder.add_view(projects_model_view, "Projects", 
+    icon="fa-folder-open",
+    category="Projects")
+
+appbuilder.add_view(resources_model_view, "Resources",
+    icon="fa-folder-open-o",
+    category="Projects")
+
+appbuilder.add_view(resources_availability_model_view,'resAvail',
+    icon="fa-folder-open-o",
+    category="Projects")
+
+appbuilder.add_view(resources_booking_model_view,'resBooking',
+    icon="fa-folder-open-o",
+    category="Projects")
