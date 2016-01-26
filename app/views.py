620431e49e2d8efe1e70f5ecd8eb1894f794e7ef
Sampath Kumar <sam1990kumar@gmail.com>
2016-01-27 00:12:49 +0530

    formating code and re-organise a bit

--- app/views.py
+++ app/views.py
@@ -26,9 +26,23 @@ def get_calendar_data(model_name):
             for item in db_data]
     return data
 
-class MyView(BaseView):
+class project(BaseView):
 
-    default_view = 'availability'
+    default_view = 'summary'
+
+    @expose('/summary/<int:project_id>')
+    @has_access
+    def summary(self, project_id):
+        '''
+            Showcase Details of project
+
+            * Project basic details
+            * Project creation details
+            * Resources attached details
+            * Users tagged for resource
+            * Requests details
+        '''
+        return self.render_template('base.html',param1='project id : %s' % project_id)
 
     @expose('/availability/<int:project_id>')
     @has_access
@@ -38,11 +52,10 @@ class MyView(BaseView):
                 cal_header='project(%r) Resource Availability Chart' % project_id,
                 cal_data=get_calendar_data(resources_availability))
 
-    @expose('/method2/<int:project_id>')
+    @expose('/bookings/<int:project_id>')
     @has_access
-    def method2(self, project_id):
+    def bookings(self, project_id):
         # Requests for a Project resources
-        param1 = 'Goodbye %s' % (project_id)
         return self.render_template('test.html',
                 cal_header='project(%r) Resource Request/Booking Chart' % project_id,
                 cal_data=get_calendar_data(resources_booking))
@@ -50,11 +63,9 @@ class MyView(BaseView):
     @expose('/method3/<string:param1>')
     @has_access
     def method3(self, param1):
-        # do something with param1
-        # and render template with param
         param1 = 'Goodbye %s' % (param1)
         self.update_redirect()
-        return self.render_template('method3.html',param1=param1)
+        return self.render_template('base.html',param1=param1)
 
     @expose('/method4/')
     @has_access
@@ -68,9 +79,9 @@ class MyView(BaseView):
         self.update_redirect()
         test_data = [
                 # 
-                ('title','url','2016-01-12T10:30:00','2016-01-12T11:30:00'),
-                ('title','url','2016-01-12T10:30:00','2016-01-12T12:30:00'),
-                ('title','url','2016-01-12T11:30:00','2016-01-12T12:30:00'),
+                ('title','url','2016-01-12T10:30:00','2016-01-12T11:30:00', ''),
+                ('title','url','2016-01-12T10:30:00','2016-01-12T12:30:00', ''),
+                ('title','url','2016-01-12T11:30:00','2016-01-12T12:30:00', ''),
                 ]
         return self.render_template('test.html', 
                 cal_header='test page',
@@ -112,13 +123,6 @@ class projects_model_view(ModelView):
             resources_model_view,
             ]
 
-
-appbuilder.add_view(MyView, "availability", href='/myview/availability/12', category='My View')
-appbuilder.add_link("Method2", href='/myview/method2/001', category='My View')
-appbuilder.add_link("Method3", href='/myview/method3/john', category='My View')
-appbuilder.add_link("Method4", href='/myview/method4', category='My View')
-appbuilder.add_link("Method5", href='/myview/method5', category='My View')
-
 ## FORM Submit
 #appbuilder.add_view(MyFormView, "My form View", icon="fa-group", \
 #        label=_('My form View'), \
@@ -132,24 +136,31 @@ appbuilder.add_link("Method5", href='/myview/method5', category='My View')
 #
 
 db.create_all()
+
 #
 #appbuilder.add_view(GroupModelView, "List Groups",
 #    icon = "fa-folder-open-o",
 #    category = "Contacts",
 #    category_icon = "fa-envelope")
 #
-appbuilder.add_view(projects_model_view, "Projects", 
-    icon="fa-folder-open",
-    category="Projects")
 
-appbuilder.add_view(resources_model_view, "Resources",
-    icon="fa-folder-open-o",
-    category="Projects")
 
-appbuilder.add_view(resources_availability_model_view,'resAvail',
-    icon="fa-folder-open-o",
-    category="Projects")
 
-appbuilder.add_view(resources_booking_model_view,'resBooking',
-    icon="fa-folder-open-o",
-    category="Projects")
+################## Tab Menu : Project ##################
+appbuilder.add_view(projects_model_view, "Projects", icon="fa-folder-open", category="Projects")
+appbuilder.add_view(resources_model_view, "Resources", icon="fa-folder-open-o", category="Projects")
+appbuilder.add_view(resources_availability_model_view,'resAvail', icon="fa-folder-open-o", category="Projects")
+appbuilder.add_view(resources_booking_model_view,'resBooking', icon="fa-folder-open-o", category="Projects")
+
+
+################## Tab Menu : Check ##################
+appbuilder.add_view(project, "Summary", href='/project/summary/12', category='Check')
+appbuilder.add_link("Availability", href='/project/availability/001', category='Check')
+appbuilder.add_link("Bookings", href='/project/bookings/001', category='Check')
+appbuilder.add_link("Method3", href='/project/method3/john', category='Check')
+appbuilder.add_link("Method4", href='/project/method4', category='Check')
+appbuilder.add_link("Method5", href='/project/method5', category='Check')
+
+
+
+
